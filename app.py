9f1afe4d5131f9408cbdae912c0bf2db9298b099
Ignus70 <systems@ber.co.za>
2024-06-11 15:08:29 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,148 @@
+import streamlit as st
+from database import create_connection, add_product, add_transaction, load_products, load_trans_types, load_data, get_balances, get_transactions, get_recon_data, add_customer, validate_login, hash_password
+import pandas as pd
+
+if 'product_entries' not in st.session_state:
+    st.session_state.product_entries = [{'product_id': None, 'qty': 1}]
+
+# Initialize session state
+if 'logged_in' not in st.session_state:
+    st.session_state['logged_in'] = False
+if 'user_id' not in st.session_state:
+    st.session_state['user_id'] = None
+
+def load_user_credentials():
+    conn = create_connection('stock_control.db')
+    try:
+        cur = conn.cursor()
+        cur.execute('SELECT Email, Password FROM tbl_Customer')
+        rows = cur.fetchall()
+        credentials = {'usernames': {}}
+        for row in rows:
+            credentials['usernames'][row[0]] = {'name': row[0], 'password': row[1]}
+        return credentials
+    finally:
+        conn.close()
+
+# Sidebar menu based on login state
+if st.session_state['logged_in']:
+    st.sidebar.title(f"Welcome, User {st.session_state['user_id']}")
+    option = st.sidebar.selectbox('Choose option:', ['Transaksie', 'Report', 'Nuwe Produk', 'Logout'])
+else:
+    st.sidebar.title('Menu')
+    option = st.sidebar.selectbox('Choose option:', ['Login', 'Sign Up'])
+
+if option == 'Login':
+    st.header('Login')
+    login_email = st.text_input('Email')
+    login_password = st.text_input('Password', type='password')
+    if st.button('Login'):
+        user_id = validate_login(login_email, login_password)
+        if user_id:
+            st.session_state['user_id'] = user_id
+            st.session_state['logged_in'] = True
+            st.success('Login successful!')
+            st.experimental_rerun()
+        else:
+            st.error('Invalid email or password')
+
+
+elif option == 'Sign Up':
+    st.header('Sign Up')
+    signup_name = st.text_input('Name')
+    signup_surname = st.text_input('Surname')
+    signup_email = st.text_input('Email')
+    signup_password = st.text_input('Password', type='password')
+    signup_password_confirm = st.text_input('Confirm Password', type='password')
+    if st.button('Sign Up'):
+        if signup_email.endswith('@ber.co.za'):
+            if signup_password == signup_password_confirm:
+                hashed_password = hash_password(signup_password)
+                user_id = add_customer(signup_name, signup_surname, signup_email, hashed_password)
+                if user_id:
+                    st.success('You have successfully signed up! Please log in.')
+                    st.experimental_rerun()
+                else:
+                    st.error('Error signing up. Please try again.')
+            else:
+                st.error('Passwords do not match')
+        else:
+            st.error('Invalid email domain. You do not belong to Bergendal.')
+
+elif option == 'Logout':
+    st.session_state['logged_in'] = False
+    st.session_state['user_id'] = None
+    st.success('Logged out successfully!')
+    st.experimental_rerun()
+
+elif 'logged_in' in st.session_state and st.session_state['logged_in']:
+    if option == 'Transaksie':
+        st.header('Create a Transaction')
+        trans_types, product_list = load_data()
+        trans_type_id = st.selectbox('Transaction Type', trans_types, format_func=lambda x: x[1])
+
+        # Buttons to add or remove products
+        col1, col2 = st.columns(2)
+        with col1:
+            if st.button('Add another product'):
+                st.session_state.product_entries.append({'product_id': None, 'qty': 1})
+        with col2:
+            if st.button('Remove last product') and len(st.session_state.product_entries) > 1:
+                st.session_state.product_entries.pop()
+
+        # Dynamically manage product selections
+        with st.form("transaction_form"):
+            # Loop through product entries and create input fields for each
+            for i, entry in enumerate(st.session_state.product_entries):
+                cols = st.columns([3, 1])
+                with cols[0]:
+                    entry['product_id'] = st.selectbox('Select Product', product_list, format_func=lambda x: x[1], key=f"product_{i}")
+                with cols[1]:
+                    entry['qty'] = st.number_input('Quantity', value=entry['qty'], min_value=1, key=f"qty_{i}")
+
+            # Form submission button
+            submitted = st.form_submit_button("Submit Transaction")
+            if submitted:
+                product_ids = [entry['product_id'][0] for entry in st.session_state.product_entries]
+                quantities = [entry['qty'] for entry in st.session_state.product_entries]
+                try:
+                    transaction_id = add_transaction(trans_type_id[0], product_ids, quantities)
+                    if transaction_id:
+                        st.success('Transaction recorded successfully!')
+                        st.session_state.product_entries = [{'product_id': None, 'qty': 1}]  # Reset product entries after successful submission
+                    else:
+                        st.error('Failed to record transaction')
+                except ValueError as ve:
+                    st.error(str(ve))
+
+
+    elif option == 'Nuwe Produk':
+        st.header('Add a New Product')
+        product_name = st.text_input('Product Name')
+        if st.button('Add Product'):
+            add_product(product_name)
+            st.success('Product added successfully!')
+
+    elif option == 'Report':
+        st.header('Product Balances')
+        view_mode = st.radio("View", ['Balans', 'Transaksie', 'Recon'])
+
+        if view_mode == 'Balans':
+            balances = get_balances()
+            df_balances = pd.DataFrame(balances, columns=['Product', 'Balance'])
+            st.write("Current Product Balances:")
+            st.table(df_balances)
+        elif view_mode == 'Transaksie':
+            transactions = get_transactions()
+            df_transactions = pd.DataFrame(transactions, columns=['Product', 'Transaction Type', 'Quantity', 'Date', 'Name'])
+            st.write("Transaction Details:")
+            st.table(df_transactions)
+        elif view_mode == 'Recon':
+            st.write("Recon View:")
+            recon_data = get_recon_data()
+            df_recon = pd.DataFrame(recon_data, columns=['Product', 'transdate_Start', 'transdate_End', 'Qty_In', 'Qty_Uit', 'Previous Balance', 'Latest Balance', 'Deviation'])
+            st.write("Product Recon Balances:")
+            st.table(df_recon)
+
+else:
+    st.warning('Please log in to access this section.')
---
+++ database.py
@@ -0,0 +1,251 @@
+import sqlite3
+from datetime import datetime
+import hashlib
+import streamlit as st
+
+# Function to connect to the SQLite database
+def create_connection(db_file='stock_control.db'):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+    return conn
+
+# Common function to execute a query and fetch all results
+def execute_query_fetch_all(conn, query, params=()):
+    try:
+        cur = conn.cursor()
+        cur.execute(query, params)
+        return cur.fetchall()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+    return []
+
+# Function to add a new product
+def add_product(product_name):
+    conn = create_connection('stock_control.db')
+    if conn:
+        try:
+            with conn:
+                sql = '''INSERT INTO tbl_Product(Product) VALUES(?)'''
+                cur = conn.cursor()
+                cur.execute(sql, (product_name,))
+                conn.commit()
+                return cur.lastrowid
+        except sqlite3.Error as e:
+            print(f"An error occurred while adding product: {e}")
+        finally:
+            conn.close()
+    return None
+
+# Function to add a transaction
+def add_transaction(trans_type_id, product_ids, quantities):
+    conn = create_connection('stock_control.db')
+    customer_id = st.session_state.get('user_id')  # Get the logged-in user's ID from session state
+    if not customer_id:
+        raise ValueError("No logged-in user found")
+
+    if conn:
+        try:
+            with conn:
+                transaction_sql = '''INSERT INTO tbl_Transaction(TransType_ID_FK, DateTime, Customer_ID_FK) VALUES(?,?,?)'''
+                cur = conn.cursor()
+                cur.execute(transaction_sql, (trans_type_id, datetime.now(), customer_id))
+                transaction_id = cur.lastrowid
+
+                product_transaction_sql = '''INSERT INTO tbl_ProductTransaction(Transaction_ID_FK, Product_ID_FK, Qty) VALUES(?,?,?)'''
+                cur.executemany(product_transaction_sql, [(transaction_id, pid, qty) for pid, qty in zip(product_ids, quantities)])
+                conn.commit()
+                return transaction_id
+        except sqlite3.Error as e:
+            print(f"An error occurred while adding transaction: {e}")
+        finally:
+            conn.close()
+    return None
+
+# Function to load transaction types
+def load_trans_types():
+    conn = create_connection('stock_control.db')
+    return execute_query_fetch_all(conn, 'SELECT TransType_ID, TransType FROM tbl_TransType')
+
+# Function to load products
+def load_products():
+    conn = create_connection('stock_control.db')
+    return execute_query_fetch_all(conn, 'SELECT Product_ID, Product FROM tbl_Product ORDER BY Product ASC')
+
+def load_data():
+    return load_trans_types(), load_products()
+
+# Function to get balances
+def get_balances():
+    conn = create_connection('stock_control.db')
+    if conn:
+        try:
+            query = '''
+            WITH LatestStocktake AS (
+                SELECT pt.Product_ID_FK, 
+                       MAX(tr.DateTime) AS LatestStocktakeDate
+                FROM tbl_ProductTransaction pt
+                JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+                JOIN tbl_TransType tt ON tr.TransType_ID_FK = tt.TransType_ID
+                WHERE tt.TransType = 'Stock Take'
+                GROUP BY pt.Product_ID_FK
+            ),
+            StocktakeBalance AS (
+                SELECT pt.Product_ID_FK,
+                       SUM(pt.Qty) AS StocktakeQty
+                FROM tbl_ProductTransaction pt
+                JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+                JOIN tbl_TransType tt ON tr.TransType_ID_FK = tt.TransType_ID
+                JOIN LatestStocktake lst ON pt.Product_ID_FK = lst.Product_ID_FK
+                                         AND tr.DateTime = lst.LatestStocktakeDate
+                GROUP BY pt.Product_ID_FK
+            )
+            SELECT p.Product,
+                   COALESCE(sb.StocktakeQty, 0) +
+                   SUM(CASE WHEN t.TransType = 'In' THEN pt.Qty
+                            WHEN t.TransType = 'Uit' THEN -pt.Qty
+                            ELSE 0 END) AS Balance
+            FROM tbl_Product p
+            LEFT JOIN tbl_ProductTransaction pt ON p.Product_ID = pt.Product_ID_FK
+            LEFT JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+            LEFT JOIN tbl_TransType t ON tr.TransType_ID_FK = t.TransType_ID
+            LEFT JOIN StocktakeBalance sb ON p.Product_ID = sb.Product_ID_FK
+            WHERE tr.DateTime > COALESCE((SELECT MAX(lst.LatestStocktakeDate) FROM LatestStocktake lst WHERE lst.Product_ID_FK = p.Product_ID), '1970-01-01')
+            OR t.TransType = 'Stock Take'
+            GROUP BY p.Product
+            '''
+            return execute_query_fetch_all(conn, query)
+        finally:
+            conn.close()
+    return []
+
+# Function to get transactions
+def get_transactions():
+    conn = create_connection('stock_control.db')
+    query = '''
+    SELECT p.Product, t.TransType, pt.Qty, tr.DateTime, 
+           c.CustomerName || ' ' || c.CustomerSurname AS Name
+    FROM tbl_ProductTransaction pt
+    JOIN tbl_Product p ON pt.Product_ID_FK = p.Product_ID
+    JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+    JOIN tbl_TransType t ON tr.TransType_ID_FK = t.TransType_ID
+    JOIN tbl_Customer c ON tr.Customer_ID_FK = c.Customer_ID
+    '''
+    return execute_query_fetch_all(conn, query)
+
+
+# Function to get recon data
+def get_recon_data():
+    conn = create_connection('stock_control.db')
+    if conn:
+        try:
+            query = '''
+            WITH LatestStocktake AS (
+                SELECT pt.Product_ID_FK, 
+                       MAX(tr.DateTime) AS LatestStocktakeDate
+                FROM tbl_ProductTransaction pt
+                JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+                JOIN tbl_TransType tt ON tr.TransType_ID_FK = tt.TransType_ID
+                WHERE tt.TransType = 'Stock Take'
+                GROUP BY pt.Product_ID_FK
+            ),
+            PreviousStocktake AS (
+                SELECT pt.Product_ID_FK, 
+                       MAX(tr.DateTime) AS PreviousStocktakeDate
+                FROM tbl_ProductTransaction pt
+                JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+                JOIN tbl_TransType tt ON tr.TransType_ID_FK = tt.TransType_ID
+                WHERE tt.TransType = 'Stock Take'
+                AND tr.DateTime < (SELECT MAX(LatestStocktakeDate) FROM LatestStocktake WHERE Product_ID_FK = pt.Product_ID_FK)
+                GROUP BY pt.Product_ID_FK
+            ),
+            StocktakeBalance AS (
+                SELECT pt.Product_ID_FK,
+                       SUM(pt.Qty) AS StocktakeQty,
+                       tr.DateTime
+                FROM tbl_ProductTransaction pt
+                JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+                JOIN tbl_TransType tt ON tr.TransType_ID_FK = tt.TransType_ID
+                WHERE tt.TransType = 'Stock Take'
+                GROUP BY pt.Product_ID_FK, tr.DateTime
+            ),
+            TransactionsInRange AS (
+                SELECT pt.Product_ID_FK,
+                       SUM(CASE WHEN t.TransType = 'In' THEN pt.Qty ELSE 0 END) as Qty_In,
+                       SUM(CASE WHEN t.TransType = 'Uit' THEN pt.Qty ELSE 0 END) as Qty_Uit
+                FROM tbl_ProductTransaction pt
+                JOIN tbl_Transaction tr ON pt.Transaction_ID_FK = tr.Transaction_ID
+                JOIN tbl_TransType t ON tr.TransType_ID_FK = t.TransType_ID
+                JOIN LatestStocktake lst ON pt.Product_ID_FK = lst.Product_ID_FK
+                JOIN PreviousStocktake pst ON pt.Product_ID_FK = pst.Product_ID_FK
+                WHERE tr.DateTime > pst.PreviousStocktakeDate AND tr.DateTime <= lst.LatestStocktakeDate
+                GROUP BY pt.Product_ID_FK
+            )
+            SELECT p.Product,
+                   pst.PreviousStocktakeDate AS transdate_Start,
+                   lst.LatestStocktakeDate AS transdate_End,
+                   trir.Qty_In,
+                   trir.Qty_Uit,
+                   COALESCE(pstb.StocktakeQty, 0) AS PreviousBalance,
+                   COALESCE(lstb.StocktakeQty, 0) AS LatestBalance,
+                   (COALESCE(pstb.StocktakeQty, 0) + COALESCE(trir.Qty_In, 0) - COALESCE(trir.Qty_Uit, 0) - COALESCE(lstb.StocktakeQty, 0)) AS Deviation
+            FROM tbl_Product p
+            LEFT JOIN LatestStocktake lst ON p.Product_ID = lst.Product_ID_FK
+            LEFT JOIN PreviousStocktake pst ON p.Product_ID = pst.Product_ID_FK
+            LEFT JOIN StocktakeBalance pstb ON pst.Product_ID_FK = pstb.Product_ID_FK AND pst.PreviousStocktakeDate = pstb.DateTime
+            LEFT JOIN StocktakeBalance lstb ON lst.Product_ID_FK = lstb.Product_ID_FK AND lst.LatestStocktakeDate = lstb.DateTime
+            LEFT JOIN TransactionsInRange trir ON p.Product_ID = trir.Product_ID_FK
+            WHERE lst.LatestStocktakeDate IS NOT NULL
+            '''
+            return execute_query_fetch_all(conn, query)
+        finally:
+            conn.close()
+    return []
+
+# Function to add a new customer
+def add_customer(name, surname, email, hashed_password):
+    conn = create_connection('stock_control.db')
+    if conn:
+        try:
+            with conn:
+                sql = '''INSERT INTO tbl_Customer(CustomerName, CustomerSurname, Email, Password) VALUES(?,?,?,?)'''
+                cur = conn.cursor()
+                cur.execute(sql, (name, surname, email, hashed_password))
+                conn.commit()
+                return cur.lastrowid
+        except sqlite3.Error as e:
+            print(f"An error occurred while adding customer: {e}")
+        finally:
+            conn.close()
+    return None
+
+# Function to validate login
+def validate_login(email, password):
+    conn = create_connection('stock_control.db')
+    if conn:
+        try:
+            cur = conn.cursor()
+            cur.execute('SELECT Customer_ID, Password FROM tbl_Customer WHERE Email = ?', (email,))
+            row = cur.fetchone()
+            if row and hashlib.sha256(password.encode()).hexdigest() == row[1]:
+                return row[0]
+        except sqlite3.Error as e:
+            print(f"An error occurred while validating login: {e}")
+        finally:
+            conn.close()
+    return None
+
+# Function to hash passwords
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+import sqlite3
+
+# Connect to SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('stock_control.db')
+c = conn.cursor()
+
+
